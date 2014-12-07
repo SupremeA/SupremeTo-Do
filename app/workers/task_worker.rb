64359aaebb8500e18a0ec887d478cd75ef681d0c
Art Supreme V <supreme036@gmail.com>
2014-12-07 01:32:31 +0000

    fix task_worker error

--- app/workers/task_worker.rb
+++ app/workers/task_worker.rb
@@ -1,7 +1,7 @@
 class TaskWorker
   include Sidekiq::Worker
 
-  def perform(rake, 5)
-      rake delete_items
-    end
-  end
+#  def perform(rake, 5)
+#      rake delete_tasks
+#  end
+end
--- lib/tasks/delete_task_rake.rb
+++ lib/tasks/delete_task_rake.rb
@@ -1,3 +1,4 @@
+
 desc "Delete task after 7 days"
 task delete_tasks: :environment do
   Task.where("created_at <= ?", Time.now - 7.days).destroy_all
